55c53a2af66462684d8abd6198ace2b276d47f93
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-06-07 23:33:03 +0300

    done HW2

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+    data: Array<AffairType>
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (_id: number) => void
 }
 
 function Affairs(props: AffairsPropsType) {
     const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+        <Affair
+            key={a._id}
             affair={a}
             deleteAffairCallback={props.deleteAffairCallback}
         />
     ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+    const setAll = () => props.setFilter('all');
+    const setHigh = () => props.setFilter('high');
+    const setMiddle = () => props.setFilter('middle');
+    const setLow = () => props.setFilter('low');
 
     return (
-        <div>
+        <table>
+            <tr>
+                <th>name</th>
+                <th>priority</th>
+            </tr>
 
             {mappedAffairs}
 
@@ -31,7 +35,7 @@ function Affairs(props: AffairsPropsType) {
             <button onClick={setHigh}>High</button>
             <button onClick={setMiddle}>Middle</button>
             <button onClick={setLow}>Low</button>
-        </div>
+        </table>
     )
 }
 
