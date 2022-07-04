59c1a63ffd9c5e4705a2ec94a9b13b557b41dfd9
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-07-04 16:03:00 +0300

    done HW5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from 'react-router-dom';
+import {PATH} from './Pages';
+import style from './Header.module.css';
 
 function Header() {
+
+    const [view, setView] = useState<boolean>(false)
+
+    const onMouseHandler = () => {
+        setView(!view)
+    }
+
+    const classNameMenu = view ? style.menu + ' ' + style.menuShow : style.menu;
+
     return (
         <div>
-            // add NavLinks
-
+            <nav>
+                <ul className={classNameMenu} onMouseLeave={onMouseHandler}>
+                    <li><NavLink to={PATH.PRE_JUNIOR}>Pre junior</NavLink></li>
+                    <li><NavLink to={PATH.JUNIOR}>Junior</NavLink></li>
+                    <li><NavLink to={PATH.JUNIOR_PLUS}>Junior+</NavLink></li>
+                </ul>
+                <div onMouseOver={onMouseHandler} className={style.box}>+</div>
+            </nav>
         </div>
     )
 }
