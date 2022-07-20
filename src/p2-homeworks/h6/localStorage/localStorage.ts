85fcca8cdb9f2daf049b77e188d34697a01cfd5c
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-07-20 14:39:50 +0300

    done HW6

--- src/p2-homeworks/h6/localStorage/localStorage.ts
+++ src/p2-homeworks/h6/localStorage/localStorage.ts
@@ -12,16 +12,16 @@ export function restoreState<T>(key: string, defaultState: T) {
     if (stateAsString !== null) state = JSON.parse(stateAsString) as T
     return state
 }
-
-// ---------------------------------------------------------------------------------------------------------------
-// пример использования:
-type StateType = {
-    x: string
-    y: number
-}
-
-// сохраняем объект типа StateType в ячейке 'test'
-saveState<StateType>('test', {x: 'A', y: 1})
-
-// получем в переменную state объект из ячейки 'test' или дэфолтный объект если ячейка пуста
-const state: StateType = restoreState<StateType>('test', {x: '', y: 0})
+//
+// // ---------------------------------------------------------------------------------------------------------------
+// // пример использования:
+// type StateType = {
+//     x: string
+//     y: number
+// }
+//
+// // сохраняем объект типа StateType в ячейке 'test'
+// saveState<StateType>('test', {x: 'A', y: 1})
+//
+// // получем в переменную state объект из ячейки 'test' или дэфолтный объект если ячейка пуста
+// const state: StateType = restoreState<StateType>('test', {x: '', y: 0})
