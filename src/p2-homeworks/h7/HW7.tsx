745b97c35de6f0aed2817562bc8556d2a94eabd6
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-07-23 22:19:52 +0300

    done HW7

--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -5,14 +5,12 @@ import SuperRadio from './common/c6-SuperRadio/SuperRadio'
 const arr = ['x', 'y', 'z']
 
 function HW7() {
-    const [value, onChangeOption] = useState(arr[1])
+    const [value, onChangeOption] = useState<string>(arr[1])
 
     return (
         <div>
             <hr/>
             homeworks 7
-
-            {/*should work (должно работать)*/}
             <div>
                 <SuperSelect
                     options={arr}
@@ -28,7 +26,6 @@ function HW7() {
                     onChangeOption={onChangeOption}
                 />
             </div>
-
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperSelect/>*/}
--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -3,8 +3,8 @@ import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react
 type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
 
 type SuperSelectPropsType = DefaultSelectPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+    options?: string[]
+    onChangeOption?: (option: string) => void
 }
 
 const SuperSelect: React.FC<SuperSelectPropsType> = (
@@ -14,10 +14,18 @@ const SuperSelect: React.FC<SuperSelectPropsType> = (
         ...restProps
     }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+    const mappedOptions: JSX.Element[] = options ? options?.map((o, i) => {
+        return (
+            <option key={o + '-' + i}>
+                {o}
+            </option>
+        )
+    })
+    : []; // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        // onChange, onChangeOption
+        onChange && onChange(e);
+        onChangeOption && onChangeOption(e.currentTarget.value);
     }
 
     return (
